a3d918678d406c7be76bf14249212b8b73edbe97
leosturmer <leo@email.com>
2025-09-19 15:05:30 -0300

    main funcionável

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import view
+from view import (TelaProdutos)
 
 from textual.app import (App, ComposeResult)
+from textual.binding import (Binding)
 
-BINDINGS = []
 
 class NizeApp(App):
-    def compose(self) -> ComposeResult:
-        self.push_screen('')
+    
+    SCREENS = {
+        'tela_produtos': TelaProdutos
+    }
+
+
+    def on_mount(self) -> ComposeResult:
+        self.push_screen('tela_produtos')
+
+if __name__ == "__main__":
+    app = NizeApp()
+    app.run()
